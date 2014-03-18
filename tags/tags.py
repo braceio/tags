a7d10d8e5952af74d75eb8622ee1e04eff86a1fc
Cole Krumbholz <colevscode@Coles-MacBook-Air.local>
2014-03-18 00:10:24 -0700

    readme tweaks

--- tags/tags.py
+++ tags/tags.py
@@ -24,8 +24,35 @@ def _is(path, body=u'', context={}):
     return body if path == context.get('filename') else u''
 
 
-# ---- Add your own tags here! -----
+# ---- Add your custom tags here! -----
+
+# Example:
+
+# @lang.add_tag
+# def print3x(style, body=u'', context={}):
+#     ''' A tag that appends 3 copies of its body '''
+#     result = body + body + body
+#     if style == "bold":
+#         result = u'<b>' + result + u'</b>'
+#     return result
+
+# Notes:
+
+# - positional arguments define the tag's required arguments.
+# - If you specify a `body` keyword argument, then the tag will require a body.
+# - All tag functions must accept a `context` keyword argument. 
+
+# You can also define tags that accept a variable argument list like so:
+
+# @lang.add_tag
+# def whatever(*args, **kwargs):
+#     return str(len(args))
 
 
 def render(content, filename=u'', rootdir=u'.'):
+    ''' 
+    Renders a content string containing template code into an output string. 
+    Uses the tags specified above. Filename and rootdir are added to the 
+    context passed to the tag functions.
+    '''
     return lang.parse(content, filename=filename, rootdir=rootdir)
